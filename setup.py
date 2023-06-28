2cc89bb0c761ac04cedf25b495500ffa8ad4866e
liyang wang <liyang.wang@ibm.com>
2023-06-28 17:21:11 -0400

    chore(requirements): update deps

--- setup.py
+++ setup.py
@@ -5,23 +5,22 @@ handles the backup process itself as well as upload and error notification.
 """
 
 from os import path
-from setuptools import setup, find_packages
+
+from setuptools import find_packages, setup
 
 HERE = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(HERE, 'README.md')) as f:
+with open(path.join(HERE, "README.md")) as f:
     LONG_DESCRIPTION = f.read()
 
 setup(
     name="backwork",
-    version="0.3.0",
+    version="0.3.1",
     description="Backup made easy.",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/ibm/backwork",
-    author="Luiz Aoqui",
-    author_email="laoqui@ca.ibm.com",
     license="Apache 2",
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -31,15 +30,11 @@ setup(
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 2 :: Only",
-        'License :: OSI Approved :: Apache Software License',
+        "License :: OSI Approved :: Apache Software License",
         "Topic :: Database",
         "Topic :: System :: Archiving :: Backup",
-        "Topic :: Utilities"
+        "Topic :: Utilities",
     ],
     packages=find_packages(),
-    entry_points={
-        "console_scripts": [
-            "backwork=backwork:main"
-        ]
-    }
+    entry_points={"console_scripts": ["backwork=backwork:main"]},
 )
